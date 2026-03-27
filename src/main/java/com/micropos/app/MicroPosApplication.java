8af8a47e3b52dee47f5b65ea3eef8b676cad8c57
JaimePrada11 <jandrespradato.11@gmail.com>
2026-03-27 18:22:26 -0500

    feat :tada: Creación del proyecto

---
+++ src/main/java/com/micropos/app/MicroPosApplication.java
@@ -0,0 +1,13 @@
+package com.micropos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/micropos/app/MicroPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micropos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
