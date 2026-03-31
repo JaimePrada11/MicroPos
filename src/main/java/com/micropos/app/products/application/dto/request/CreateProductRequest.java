4336faed8079b1b9a5fc2cf66401a58d6811269d
JaimePrada11 <jandrespradato.11@gmail.com>
2026-03-31 12:41:18 -0500

    feat :sparkles: Creación de los dtos de Product
    Se crearon los dtos necesarios para el crear un producto como la respuesta de este

--- src/main/java/com/micropos/app/products/application/dto/request/CreateProductRequest.java
+++ src/main/java/com/micropos/app/products/application/dto/request/CreateProductRequest.java
@@ -1,4 +1,15 @@
 package com.micropos.app.products.application.dto.request;
 
-public class CreateProductRequest {
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Builder
+public record CreateProductRequest (
+       Long id,
+       String name,
+       String description,
+       BigDecimal price)
+{
 }
--- src/main/java/com/micropos/app/products/application/dto/response/ProductResponse.java
+++ src/main/java/com/micropos/app/products/application/dto/response/ProductResponse.java
@@ -1,4 +1,35 @@
 package com.micropos.app.products.application.dto.response;
 
-public class ProductResponse {
+import com.micropos.app.products.domain.enums.ProductCategory;
+import com.micropos.app.products.domain.enums.ProductStatus;
+import com.micropos.app.products.domain.model.Product;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record ProductResponse(
+        Long id,
+        String sku,
+        String name,
+        String description,
+        String image,
+        ProductCategory category,
+        ProductStatus status,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+
+    public static ProductResponse from(Product p) {
+        return new ProductResponse(
+                p.getId(),
+                p.getSku(),
+                p.getName(),
+                p.getDescription(),
+                p.getImage(),
+                p.getCategory(),
+                p.getStatus(),
+                p.getCreatedAt(),
+                p.getUpdatedAt()
+        );
+    }
 }
