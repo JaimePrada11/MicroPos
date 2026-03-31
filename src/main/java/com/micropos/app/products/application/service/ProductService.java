3efa846f522ff53a68295d47b6a0b5dd789d5946
JaimePrada11 <jandrespradato.11@gmail.com>
2026-03-31 13:27:59 -0500

    feat :rocket: Implementar todos los métodos en ProductServiceImpl
    Se implementan todos los métodos del ProductServiceImpl:
    - createProduct, updateProduct, deleteProduct, getProductById
    - Manejo de excepciones específicas (ProductNotFoundException)

--- src/main/java/com/micropos/app/products/application/service/ProductService.java
+++ src/main/java/com/micropos/app/products/application/service/ProductService.java
@@ -1,15 +1,28 @@
 package com.micropos.app.products.application.service;
 
 import com.micropos.app.products.application.dto.request.CreateProductRequest;
+import com.micropos.app.products.application.dto.request.UpdateProductRequest;
 import com.micropos.app.products.domain.model.Product;
 import com.micropos.app.products.domain.repository.ProductRepository;
 import com.micropos.app.products.infrastructure.mapper.ProductMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
 public interface ProductService {
 
-    Product save(CreateProductRequest request);
+    Product create(CreateProductRequest request);
+
+    Product findById(Long id);
+
+    Product findBySku(String sku);
+
+    Page<Product> findAll(Pageable pageable);
+
+    Product update(Long id, UpdateProductRequest request);
+
+    void delete(Long id);
 
 }
--- src/main/java/com/micropos/app/products/application/service/ProductServiceImp.java
+++ src/main/java/com/micropos/app/products/application/service/ProductServiceImp.java
@@ -1,10 +1,17 @@
 package com.micropos.app.products.application.service;
 
 import com.micropos.app.products.application.dto.request.CreateProductRequest;
+import com.micropos.app.products.application.dto.request.UpdateProductRequest;
+import com.micropos.app.products.domain.SkuGenerator;
+import com.micropos.app.products.domain.exception.ProductAlreadyExistsException;
+import com.micropos.app.products.domain.exception.ProductNotFoundException;
 import com.micropos.app.products.domain.model.Product;
 import com.micropos.app.products.domain.repository.ProductRepository;
 import com.micropos.app.products.infrastructure.mapper.ProductMapper;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,17 +21,67 @@ public class ProductServiceImp implements ProductService {
     private final ProductRepository productRepository;
 
     @Override
-    public Product save(CreateProductRequest request) {
-        if(productRepository.existsByName(request.name())){
-            throw new RuntimeException("Product name already exists");
-        }
+    @Transactional
+    public Product create(CreateProductRequest request) {
 
+        String sku = SkuGenerator.generate(request.category());
+
+        if(productRepository.existsBySku(sku)) throw  new ProductAlreadyExistsException(sku);
         Product product = Product.builder()
+                .sku(sku)
                 .name(request.name())
                 .description(request.description())
                 .price(request.price())
+                .image(request.image())
+                .category(request.category())
                 .build();
 
         return productRepository.save(product);
+    }
+
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
+    }
+
+    @Override
+    public Product findBySku(String sku) {
+        return productRepository.findBySku(sku)
+                .orElseThrow(() -> new ProductNotFoundException(sku));
+    }
+
+    @Override
+    public Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Override
+    @Transactional
+    public Product update(Long id, UpdateProductRequest request) {
+        Product productExits = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
+
+        Product update = Product.builder()
+                .id(productExits.getId())
+                .sku(productExits.getSku())
+                .name(request.name())
+                .description(request.description())
+                .price(request.price())
+                .image(request.image())
+                .category(request.category())
+                .build();
+
+        return productRepository.save(update);
+    }
+
+    @Override
+    public void delete(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
+
+        product.deactive();
+        productRepository.save(product);
     }
 }
